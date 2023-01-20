79a3ff69ef7f3876a6dcbf5c773b72fd53a7aacc
mile-misan <mile.misan@nearform.com>
2023-01-21 00:07:02 +0100

    test certs

--- scripts/generate-certs.sh
+++ scripts/generate-certs.sh
@@ -1,26 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+source .envrc
 
 mkdir -p .ssl
 
-cat << EOF > .ssl/req.cnf
-[req]
-req_extensions = v3_req
-distinguished_name = req_distinguished_name
+# check if certificate expired or is going to expiry in less than 2 days
+openssl x509 -in .ssl/ca.pem -noout -checkend 172800 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Existing certificates are valid"
+else
+  echo "Creating new certificates"
 
-[req_distinguished_name]
+  cat << EOF > .ssl/req.cnf
+  [req]
+  req_extensions = v3_req
+  distinguished_name = req_distinguished_name
 
-[ v3_req ]
-basicConstraints = CA:FALSE
-keyUsage = nonRepudiation, digitalSignature, keyEncipherment
-subjectAltName = @alt_names
+  [req_distinguished_name]
 
-[alt_names]
-DNS.1 = *.kube.local
+  [ v3_req ]
+  basicConstraints = CA:FALSE
+  keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+  subjectAltName = @alt_names
+
+  [alt_names]
+  DNS.1 = *.kube.local
 EOF
 
-openssl genrsa -out .ssl/ca-key.pem 2048
-openssl req -x509 -new -nodes -key .ssl/ca-key.pem -days 30 -out .ssl/ca.pem -subj "/CN=root-ca"
+  openssl genrsa -out .ssl/ca-key.pem 2048
+  openssl req -x509 -new -nodes -key .ssl/ca-key.pem -days "${KKA_CERT_VALIDITY_DAYS}" -out .ssl/ca.pem -subj "/CN=root-ca"
 
-openssl genrsa -out .ssl/key.pem 2048
-openssl req -new -key .ssl/key.pem -out .ssl/csr.pem -subj "/CN=kube-local" -config .ssl/req.cnf
-openssl x509 -req -in .ssl/csr.pem -CA .ssl/ca.pem -CAkey .ssl/ca-key.pem -CAcreateserial -out .ssl/cert.pem -days 30 -extensions v3_req -extfile .ssl/req.cnf
+  openssl genrsa -out .ssl/key.pem 2048
+  openssl req -new -key .ssl/key.pem -out .ssl/csr.pem -subj "/CN=kube-local" -config .ssl/req.cnf
+  openssl x509 -req -in .ssl/csr.pem -CA .ssl/ca.pem -CAkey .ssl/ca-key.pem -CAcreateserial -out .ssl/cert.pem -days "${KKA_CERT_VALIDITY_DAYS}" -extensions v3_req -extfile .ssl/req.cnf
+fi
