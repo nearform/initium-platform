c276521bdec1a3f4a0c5fd468ae011b8bf0843dc
mile-misan <mile.misan@nearform.com>
2023-01-30 15:02:44 +0100

    chore: remove kubelogin token when cluster is removed

--- scripts/clean-test.sh
+++ scripts/clean-test.sh
@@ -8,3 +8,8 @@ if [ -d "$GOPATH" ]; then
   echo "Deleting ${GOPATH}..."
   sudo rm -rf $GOPATH
 fi
+
+if [ -d "$HOME/.kube/cache/oidc-login" ]; then
+  echo "Deleting $HOME/.kube/cache/oidc-login..."
+  sudo rm -rf $HOME/.kube/cache/oidc-login
+fi
