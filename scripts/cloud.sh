9be7ff9bae9ac623b88ad52a6c4e64775e48ed93
fabiolrodriguez <fabio.rodriguez@nearform.com>
2023-03-02 14:29:46 -0300

    feat: change scripts to install only argocd

--- scripts/cloud.sh
+++ scripts/cloud.sh
@@ -7,70 +7,30 @@ echo "======================================================"
 printenv | grep "KKA_.*"
 echo "======================================================"
 
-if [ "${KKA_DEPLOY_MINIMAL}" == "false" ]; then
-  # Install ArgoCD
-  kubectl create namespace argocd
-  helm repo add argo https://argoproj.github.io/argo-helm
-  helm repo update
-  helm install argocd argo/argo-cd --namespace=argocd -f ./addons/argocd/values.yaml
-  sleep 10
-  # Install apps
-  kubectl apply -f https://github.com/nearform/k8s-kurated-addons/releases/download/v0.0.1/app-of-apps.yaml
-  # Login on ArgoCD
-  kubectl config set-context --current --namespace=argocd
-  argocd login --core --name k8s-kurated-addons
-  # Ensure ArgoCD apps are all healthy and in sync
-  echo ">> Waiting for k8s-kurated-addons to be healty and in sync..."
-  while [ true ]
-  do
-    ALL_HEALTHY=true
-    readarray -t apps_health < <(argocd app get k8s-kurated-addons -o json | jq -r '.status.resources | .[]? | select(.kind | contains("Application")) | .health')
+# Install ArgoCD
+kubectl create namespace argocd
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+helm install argocd argo/argo-cd --namespace=argocd -f ./addons/argocd/values.yaml
+# Ensure ArgoCD apps are all healthy and in sync
+echo ">> Waiting for argocd to be healty and in sync..."
 
-    if (( ${#apps_health[@]} == 0 )); then
-      ALL_HEALTHY=false
+while true; do
+    # Check the status of the ArgoCD deployment
+    deployment_status=$(kubectl get deployment -n argocd argocd-server -o jsonpath='{.status.readyReplicas}')
+
+    if [ "$deployment_status" != "1" ]; then
+      echo "ArgoCD deployment is not ready."
     else
-      for status in ${apps_health[@]}
-      do
-        if [ "$status" == "Healthy" ] || [ "$status" == null ]; then
-          ALL_HEALTHY=true
-        fi
-      done
+      # Check the status of the ArgoCD pod
+      pod_status=$(kubectl get pod -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[].status.phase}')
+
+      if [ "$pod_status" != "Running" ]; then
+        echo "ArgoCD pod is not running. Current pod status: $pod_status"
+      else
+        echo "ArgoCD is installed and ready to use."
+        break;
+      fi
     fi
-
-    ALL_SYNCED=true
-    readarray -t apps_sync < <(argocd app get k8s-kurated-addons -o json | jq -r '.status.resources | .[]? | select(.kind | contains("Application")) | .status')
-    if (( ${#apps_sync[@]} == 0 )); then
-      ALL_SYNCED=false
-    else
-      for status in ${apps_sync[@]}
-      do
-        if [ "$status" != "Synced" ]; then
-          ALL_SYNCED=false
-        fi
-      done
-    fi
-
-    if [[ "$ALL_HEALTHY" == "true" && "$ALL_SYNCED" == "true" ]]; then
-      break
-    else
-      argocd app get k8s-kurated-addons
-
-      # Print the 10 last lines of logs of apps not currently healthy
-      readarray -t apps < <(argocd app get k8s-kurated-addons -o json | jq -r '.status.resources | .[]? | select(.kind | contains("Application")) | select(.status | contains("Progressing")) | .health')
-      for app in ${apps[@]}
-      do
-        echo ">> Printing last 10 log lines for $app..."
-        argocd app logs $app --tail 10
-      done
-    fi
-
-    sleep 15
-  done
-
-  # List all the installed apps
-  argocd app list
-
-  # Logout on ArgoCD
-  argocd logout k8s-kurated-addons
-  kubectl config set-context --current --namespace=default
-fi
+  sleep 30
+done
