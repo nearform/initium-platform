5ba50f8942f4a69e67cc95dd883c16f8e84a3ec3
Brendt Lucas <brendt.lucas@nearform.com>
2023-03-15 05:47:51 -0400

    run multiple tests in parallel and fix lint

--- examples/sample-podinfo-app/sample_podinfo_app_test.go
+++ examples/sample-podinfo-app/sample_podinfo_app_test.go
@@ -32,6 +32,61 @@ func TestSamplePodInfoApp(t *testing.T) {
 	k8s.KubectlApply(t, kubectlOptions, "sample-podinfo-app.yaml")
 
 	// =============================================================
+	t.Run("Parallel", func(t *testing.T) {
+		t.Run("respMsg", testResponseMessage)
+		t.Run("version", testVersion)
+		t.Run("statusCode", testStatusCode)
+	})
+
+	// =============================================================
+	k8s.KubectlDelete(t, kubectlOptions, "sample-podinfo-app.yaml")
+}
+
+func testStatusCode(t *testing.T) {
+	http_helper.HTTPDoWithRetry(
+		t,
+		"GET",
+		fmt.Sprintf("http://%s/status/504", os.Getenv("KKA_LB_ENDPOINT")),
+		nil,
+		map[string]string{"Host": "sample-podinfo-app.default.example.com"},
+		504,
+		30,
+		3*time.Second,
+		nil,
+	)
+}
+
+func testVersion(t *testing.T) {
+	out := http_helper.HTTPDoWithRetry(
+		t,
+		"GET",
+		fmt.Sprintf("http://%s/version", os.Getenv("KKA_LB_ENDPOINT")),
+		nil,
+		map[string]string{"Host": "sample-podinfo-app.default.example.com"},
+		200,
+		30,
+		3*time.Second,
+		nil,
+	)
+
+	var actual map[string]string
+	err := json.Unmarshal([]byte(out), &actual)
+	if err != nil {
+		t.Fatal("Failed to unmarshal response body", err)
+	}
+
+	actualValue, exists := actual["version"]
+
+	assert.True(t, exists)
+	assert.Equal(t, "6.3.5", actualValue)
+
+	actualValue, exists = actual["commit"]
+
+	assert.True(t, exists)
+	assert.Equal(t, "67e2c98a60dc92283531412a9e604dd4bae005a9", actualValue)
+}
+
+func testResponseMessage(t *testing.T) {
 	out := http_helper.HTTPDoWithRetry(
 		t,
 		"GET",
@@ -54,7 +109,4 @@ func TestSamplePodInfoApp(t *testing.T) {
 
 	assert.True(t, exists)
 	assert.Equal(t, "greetings from podinfo v6.3.5", actualValue)
-
-	// =============================================================
-	k8s.KubectlDelete(t, kubectlOptions, "sample-podinfo-app.yaml")
 }
